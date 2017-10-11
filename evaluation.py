b0ebc07d97f27bcdb613051b8c5916c96ed33533
wondervictor <victorchanchina@gmail.com>
2017-10-11 21:14:34 +0800

    add: train neural network

--- evaluation.py
+++ evaluation.py
@@ -6,3 +6,12 @@ from sklearn.metrics import f1_score
 def fscore(truth, pred):
     pass
 
+
+def evaluate(pred_prob, labels):
+    """
+    验证：精确率，召回率，F score
+    :param pred_prob:
+    :param labels:
+    :return:
+    """
+    pass
--- model/network.py
+++ model/network.py
@@ -10,7 +10,8 @@ Neural Network Model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch.optim as optimizer
+from torch.autograd import Variable
 
 USER_VECTOR_SIZE=10
 PRODUCT_VECTOR_SIZE=10
@@ -18,7 +19,7 @@ PRODUCT_VECTOR_SIZE=10
 
 class NeuralNetwork(nn.Module):
 
-    def __init__(self, dict_size):
+    def __init__(self):
 
         super(NeuralNetwork, self).__init__()
 
@@ -74,4 +75,130 @@ class NeuralNetwork(nn.Module):
         )
 
         return output
+
+
+def save_model(network, path):
+
+    torch.save(network, path)
+    print("Save Model to File: %s" % path)
+
+
+def load_model(path):
+
+    network = torch.load(path)
+
+    return network
+
+
+def output(network, user_self_vector, user_desc_vector, product_self_vector, product_desc_vector):
+    user_self_vector = Variable(torch.FloatTensor(user_self_vector))
+    user_desc_vector = Variable(torch.FloatTensor(user_desc_vector))
+    product_desc_vector = Variable(torch.FloatTensor(product_desc_vector))
+    product_self_vector = Variable(torch.FloatTensor(product_self_vector))
+
+    prob = network(
+        product_vector=product_self_vector,
+        user_vector=user_self_vector,
+        user_desc_vector=user_desc_vector,
+        product_desc_vector=product_desc_vector
+    )
+
+    return prob
+
+
+def train(trainset, epoch, user_feature, product_feature):
+
+    learning_rate = 0.0001
+
+    network = NeuralNetwork()
+
+    training_optimizer = optimizer.Adam(lr=learning_rate, params=network.parameters())
+
+    loss_criterion = torch.nn.CrossEntropyLoss()
+
+    for i in range(epoch):
+        for data in trainset:
+            person_id = data[0]
+            product_id = data[1]
+            label = data[-1]
+
+            user_self_vector, user_desc_vector = user_feature[person_id]
+
+            product_self_vector, product_desc_vector = product_feature[product_id]
+
+            label = Variable(torch.LongTensor([label]))
+
+            prob = output(
+                network,
+                user_self_vector,
+                user_desc_vector,
+                product_self_vector,
+                product_desc_vector
+            )
+            loss = loss_criterion(prob, label)
+
+            if i % 10:
+                print("Sample: %s: Loss: %s" % (i+1, loss.data[0]))
+
+            training_optimizer.zero_grad()
+            loss.backward()
+            training_optimizer.step()
+        save_model(network, '../model_param/neural_network_param_%s' % i)
+
+
+def test(testset, user_feature, product_feature, model_path):
+
+    network = load_model(model_path)
+
+    result = []
+    labels = []
+
+    for data in testset:
+        person_id = data[0]
+        product_id = data[1]
+        label = data[-1]
+
+        user_self_vector, user_desc_vector = user_feature[person_id]
+
+        product_self_vector, product_desc_vector = product_feature[product_id]
+
+        prob = output(
+            network,
+            user_self_vector,
+            user_desc_vector,
+            product_self_vector,
+            product_desc_vector
+        )
+
+        labels.append(label)
+        result.append(prob)
+
+    return labels, result
+
+
+def predict(predict_set, user_feature, product_feature, model_path):
+
+    network = load_model(model_path)
+    result = []
+
+    for data in predict_set:
+        person_id = data[0]
+        product_id = data[1]
+
+        user_self_vector, user_desc_vector = user_feature[person_id]
+
+        product_self_vector, product_desc_vector = product_feature[product_id]
+
+        prob = output(
+            network,
+            user_self_vector,
+            user_desc_vector,
+            product_self_vector,
+            product_desc_vector
+        )
+
+        result.append(prob)
+
+    return result
+
 
